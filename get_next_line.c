31e1dfefb7cda52c896155731b9e8556d84c14d2
Irene Puig Parnau <ipuig-pa@1-D-27.42heilbronn.de>
2024-10-21 17:32:07 +0200

    first logic

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,40 @@
 /*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 18:04:00 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2024/10/19 17:50:18 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2024/10/21 17:31:17 by ipuig-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-//falta copiar de buffer al string de malloc. 
-//falta entendre com anira agafant el que ja ha llegit. 
+size_t	str_len(char *str)
+{
+	size_t	len;
+
+	while (str)
+	{
+		str++;
+		len++;
+	}
+	return (len);
+}
+
+char	*str_copy(char *dst, const char *src)
+{
+	size_t	i;
+
+	i = 0;
+	if (dst && src)
+	{
+		while (i < str_len(dst) - 1 && src[i] != '\0')
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (dst);
+}
 
 char	*get_next_line(int fd)
 {
@@ -24,8 +50,8 @@ char	*get_next_line(int fd)
 	size_t		i;
 
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	used_buffers = 0;
-	leftover = //afegir el que queda del buffer no usat
+	leftover = NULL;
+	leftover_len = 0;
 	while (bytes_read > 0)
 	{
 		i = 0;
@@ -33,13 +59,35 @@ char	*get_next_line(int fd)
 			i++;
 		if (buffer[i] == '\n')
 		{
-			line = (char *)malloc(BUFFER_SIZE * used_buffers + i);
-			if (!line)
-				return (NULL);
+			if (leftover)
+				return ((char *)malloc(ft_strlen(leftover) + i));
+			free(leftover);
+			leftover = NULL;
 		}
 		else
+			if (leftover)
+			{
+				leftover_cpy = str_copy(leftover_cpy, leftover);
+				free(leftover);
+				leftover =(static char *)malloc(BUFFER_SIZE + leftover_len);
+				if (!leftover)
+					return (NULL);
+				str_copy(leftover, leftover_cpy);
+				strlcat(leftover, buffer, BUFFER_SIZE);
+			}
+			else
+			{
+				leftover =(static char *)malloc(BUFFER_SIZE);
+				if (!leftover)
+					return (NULL);
+				i = 0;
+				while (i <= BUFFER_SIZE)
+				{
+					leftover[i] = buffer[i]
+					i++;
+				}
+			}
 			bytes_read = read(fd, buffer, BUFFER_SIZE);
-		used_buffers++;
 	}
 	if (bytes_read == -1)
 	{
