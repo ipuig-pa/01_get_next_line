325bcee25f9ca34d01df3d8790c1b8ac1f5768f6
Irene Puig Parnau <ipuig-pa@1-D-3.42heilbronn.de>
2024-10-23 16:57:35 +0200

    logic and splitted but still not working. Header still needs to include all the functions

--- get_next_line.c
+++ get_next_line.c
@@ -6,107 +6,56 @@
 /*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 18:04:00 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2024/10/23 15:47:31 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2024/10/23 16:55:31 by ipuig-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(char *str)
-{
-	size_t	len;
-
-	len = 0;
-	if (!str)
-		return (0);
-	while (str[len] != '\0')
-		len++;
-	return (len);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-
-	i = 0;
-	if (!dst || !src)
-		return (0);
-	if (dstsize != 0)
-	{
-		while (i < dstsize - 1 && src[i] != '\0')
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	return (ft_strlen((char *)src));
-}
-
-size_t	newline_len(char *str)
-{
-	size_t	i;
-
-	i = 0;
-	if (!str)
-		return (i);
-	while (str[i] != '\n' && str[i] != '\0')
-		i++;
-	if (str[i] == '\n')
-		i++;
-	return (i);
-}
-
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-	size_t	dst_len;
-	size_t	src_len;
-
-	i = 0;
-	if (!dst || !src)
-		return (0);
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen((char *)src);
-	if (dstsize == 0)
-		return (src_len);
-	if (dstsize <= dst_len)
-		return (src_len + dstsize);
-	while (i < dstsize - dst_len - 1 && src[i] != '\0')
-	{
-		dst[i + dst_len] = src[i];
-		i++;
-	}
-	dst[i + dst_len] = '\0';
-	return (dst_len + ft_strlen((char *)src));
-}
-
 char	*new_leftover(char *leftover, size_t line_len, size_t leftover_len)
 {
 	char	*temp;
 
+	if (line_len >= leftover_len)
+	{
+		free(leftover);
+		return (NULL);
+	}
 	temp = (char *)malloc(leftover_len - line_len + 1);
 	if (!temp)
+	{
+		free(leftover);
 		return (NULL);
+	}
 	ft_strlcpy(temp, (leftover + line_len), (leftover_len - line_len));
+	free(leftover);
 	return (temp);
 }
 
-char	*line_from_leftover(char *leftover, size_t line_len, size_t leftover_len)
+char	*line_from_leftover(char *leftover, size_t line_len)
 {
 	char	*line;
+	size_t	leftover_len;
 
+	leftover_len = ft_strlen(leftover);
+	if (!leftover || line_len == 0)
+		return (NULL);
 	line = (char *)malloc(line_len + 1);
 	if (!line)
+	{
+		free(leftover);
 		return (NULL);
+	}
 	ft_strlcpy(line, leftover, line_len + 1);
 	return (line);
 }
 
-char	*add_chunk(char *leftover, char *buffer, size_t bytes_read, size_t leftover_len)
+char	*add_chunk(char *leftover, char *buffer, size_t bytes_read)
 {
-	char		*temp;
+	char	*temp;
+	size_t	leftover_len;
 
+	leftover_len = ft_strlen(leftover);
 	temp = (char *)malloc(bytes_read + leftover_len + 1);
 	if (!temp)
 		return (NULL);
@@ -123,76 +72,47 @@ char	*add_chunk(char *leftover, char *buffer, size_t bytes_read, size_t leftover
 
 char	*get_next_line(int fd)
 {
-	char		buffer[BUFFER_SIZE];
+	char		buffer[BUFFER_SIZE + 1];
 	size_t		bytes_read;
 	static char	*leftover;
 	size_t		line_len;
-	size_t		leftover_len;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	leftover_len = ft_strlen(leftover);
 	line_len = newline_len(leftover);
-	while (!(line_len < leftover_len))
+	if (line_len < ft_strlen(leftover))
+	{
+		line = line_from_leftover(leftover, line_len);
+		leftover = new_leftover(leftover, line_len, ft_strlen(leftover));
+		return (line);
+	}
+	while (1)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read > 0)
-			leftover = add_chunk(leftover, buffer, bytes_read, leftover_len);
-		else if (bytes_read == 0)
-			return(leftover);
-		else
-			return (NULL);
-		line_len = newline_len(leftover);
-		leftover_len = ft_strlen(leftover);
-	}
-	line = line_from_leftover(leftover, line_len, leftover_len);
-	leftover = new_leftover(leftover, line_len, leftover_len);
-	return (line);
-}
-
-
-/*
-
-
-		if (line_len <= leftover_len)
-		{
-			line = (char *)malloc(line_len + 1);
-			if (!line)
-				return (NULL);
-			ft_strlcpy(line, leftover, line_len + 1);
-			temp = (char *)malloc(leftover_len - line_len + 1);
-			if (!temp)
-				return (NULL);
-			ft_strlcpy(temp, (leftover + line_len), (leftover_len - line_len));
-			free(leftover);
-			leftover = (char *)malloc(leftover_len - line_len + 1);
-			if (!leftover)
-				return (NULL);
-			ft_strlcpy(leftover, temp, leftover_len - line_len + 1);
-			free(temp);
-			return (line);
-		}
-	
-	return (process_leftover);
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read > 0)
-	{
-		temp = (char *)malloc(bytes_read + leftover_len + 1);
-		if (!temp)
-			return (NULL);
-		if (leftover)
-		{
-			ft_strlcpy(temp, leftover, leftover_len + 1);
-			free(leftover);
-		}
-		ft_strlcat(temp, buffer, bytes_read + leftover_len + 1);
-		leftover = (char *)malloc(bytes_read + leftover_len + 1);
+		if (bytes_read <= 0)
+			break ;
+		buffer[bytes_read] = '\0';
+		leftover = add_chunk(leftover, buffer, bytes_read);
 		if (!leftover)
 			return (NULL);
-		ft_strlcpy(leftover, temp, bytes_read + leftover_len + 1);
-		free(temp);
-		return (process_leftover());
+		line_len = newline_len(leftover);
+		if (line_len < ft_strlen(leftover))
+		{
+			line = line_from_leftover(leftover, line_len);
+			leftover = new_leftover(leftover, line_len, ft_strlen(leftover));
+			return (line);
+		}
 	}
+	if (bytes_read == 0 && leftover && *leftover)
+	{
+		line = line_from_leftover(leftover, ft_strlen(leftover));
+		free (leftover)
+		leftover = NULL;
+		return (line);
+	}
+	if (leftover)
+		free(leftover);
+	leftover = NULL;
 	return (NULL);
 }
-*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 18:05:54 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2024/10/23 13:37:28 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2024/10/23 16:56:46 by ipuig-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,12 @@
 # include <stddef.h>
 # include <stdlib.h>
 //# define BUFFER_SIZE 1024
+
+size_t	ft_strlen(char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	newline_len(char *str);
+
+
+
 
 #endif
