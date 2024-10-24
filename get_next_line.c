b3859dd1ba5c1840d08ccc419d7b213f19f383b0
Irene Puig Parnau <ipuig-pa@1-B-13.42heilbronn.de>
2024-10-24 15:41:07 +0200

    main error solved, still some tests not working

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 18:04:00 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2024/10/24 13:38:03 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2024/10/24 15:40:12 by ipuig-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,18 +38,18 @@ char	*line_from_leftover(char *leftover, size_t line_len)
 	size_t	leftover_len;
 
 	leftover_len = ft_strlen(leftover);
-	printf("leftover_len: %zu\n", leftover_len);
-	if (!leftover || line_len == 0)
+	//printf("leftover_len: %zu\n", leftover_len);
+	if (!leftover)
 		return (NULL);
-	line = (char *)malloc(line_len + 1);
+	line = (char *)malloc(line_len + 2);
 	if (!line)
 	{
 		free(leftover);
 		return (NULL);
 	}
-	printf("line: %s\n", line);
-	ft_strlcpy(line, leftover, line_len + 1);
-	printf("line: %s\n", line);
+	//printf("line: %s\n", line);
+	ft_strlcpy(line, leftover, line_len + 2);
+	//printf("line: %s\n", line);
 	return (line);
 }
 
@@ -81,55 +81,55 @@ char	*get_next_line(int fd)
 	size_t		line_len;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	if (leftover)
 	{
-		printf("there was leftover\n");
-		printf("leftover: %s\n", leftover);
+		//printf("there was leftover\n");
+		//printf("leftover: %s\n", leftover);
 		line_len = newline_len(leftover);
-		printf("line_len: %zu\n", line_len);
-		printf("leftover_len: %zu\n", ft_strlen(leftover));
+		//printf("line_len: %zu\n", line_len);
+		//printf("leftover_len: %zu\n", ft_strlen(leftover));
 		if (line_len < ft_strlen(leftover))
 		{
-			printf("there is a new line in leftover\n");
+			//printf("there is a new line in leftover\n");
 			line = line_from_leftover(leftover, line_len);
-			printf("line: %s\n", line);
+			//printf("line: %s\n", line);
 			leftover = new_leftover(leftover, line_len, ft_strlen(leftover));
-			printf("newleftover: %s\n", leftover);
+			//printf("newleftover: %s\n", leftover);
 			return (line);
 		}
 	}
-	printf("No leftover\n");
+	//printf("No leftover\n");
 	while (1)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		printf("bytes_read: %zu\n", bytes_read);
+		//printf("bytes_read: %zu\n", bytes_read);
 		if (bytes_read <= 0)
 			break ;
 		buffer[bytes_read] = '\0';
-		printf("buffer: %s\n", buffer);
+		//printf("buffer: %s\n", buffer);
 		leftover = add_chunk(leftover, buffer, bytes_read);
-		printf("leftover: %s\n", leftover);
+		//printf("leftover: %s\n", leftover);
 		if (!leftover)
 			return (NULL);
 		line_len = newline_len(leftover);
-		printf("line_len: %zu\n", line_len);
-		printf("leftover_len: %zu\n", ft_strlen(leftover));
+		//printf("line_len: %zu\n", line_len);
+		//printf("leftover_len: %zu\n", ft_strlen(leftover));
 		if (line_len < ft_strlen(leftover))
 		{
 			line = line_from_leftover(leftover, line_len);
-			printf("line: %s\n", line);
+			//printf("line: %s\n", line);
 			leftover = new_leftover(leftover, line_len, ft_strlen(leftover));
-			printf("newleftover: %s\n", leftover);
+			//printf("newleftover: %s\n", leftover);
 			return (line);
 		}
 	}
 	if (bytes_read == 0 && leftover && *leftover)
 	{
-		printf("leftover_len: %zu\n", ft_strlen(leftover));
+		//printf("leftover_len: %zu\n", ft_strlen(leftover));
 		line = line_from_leftover(leftover, ft_strlen(leftover));
-		printf("line: %s\n", line);
+		//printf("line: %s\n", line);
 		free (leftover);
 		leftover = NULL;
 		return (line);
@@ -140,7 +140,7 @@ char	*get_next_line(int fd)
 	return (NULL);
 }
 
-#include <fcntl.h>
+/*#include <fcntl.h>
 
 int	main(void)
 {
@@ -148,10 +148,10 @@ int	main(void)
 	char	*line;
 
 	fd = open("prova.txt", 0);
-	while ((line = get_next_line(1000)))
+	while ((line = get_next_line(fd)))
 	{
 		printf("LINE_READ: %s\n", line);
 		free(line);
 	}
 	return (0);
-}
+}*/
